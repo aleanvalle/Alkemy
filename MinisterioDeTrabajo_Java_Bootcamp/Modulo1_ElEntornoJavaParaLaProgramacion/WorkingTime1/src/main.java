67a7d39a37b1ec759759085f818a7a1a2dabd405
aleanvalle <alexv4lle@gmail.com>
2024-02-05 22:31:44 -0300

    modulo1 working time

---
+++ MinisterioDeTrabajo_Java_Bootcamp/Modulo1_ElEntornoJavaParaLaProgramacion/WorkingTime1/src/main.java
@@ -0,0 +1,23 @@
+import java.util.Arrays;
+
+public class main {
+
+	public static void main(String[] args) {
+		System.out.println("¡Hola Mundo!, estoy programando en Java.");
+		int numero = 5;
+		String palabra = "hola";
+		char caracter = '%';
+		boolean condicion = 5 == 5;
+		float numeroDecimal = 5.2f;
+		int[] array = {1, 2, 3};
+		System.out.println
+		("numero: " + numero + "\n"
+		+ "palabra: " + palabra + "\n"
+		+ "caracter: " + caracter + "\n" 
+		+ "condicion: " + condicion + "\n" 
+		+ "numeroDecimal: " + numeroDecimal + "\n" 
+		+ "array: " + Arrays.toString(array) + "\n"
+		);
+	}
+
+}
---
+++ MinisterioDeTrabajo_Java_Bootcamp/Modulo1_ElEntornoJavaParaLaProgramacion/WorkingTime2/src/main.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class main {
+
+	public static void main(String[] args) {
+		int sum = sum();
+		String name = name();
+		System.out.println("Result: " + sum);
+		System.out.println("Result: " + name);
+	}
+	
+	static int sum() {
+		Scanner scan = new Scanner(System.in);
+		System.out.println("Enter a number: ");
+		int a = scan.nextInt();
+		System.out.println("Enter another number: ");
+		int b = scan.nextInt();
+		return a + b;
+	}
+	
+	static String name() {
+		Scanner scan = new Scanner(System.in);
+		System.out.println("Enter a name: ");
+		String a = scan.next();
+		return "¡Hola mundo! soy " + a + " y estoy programando en Java!";
+	}
+
+}
