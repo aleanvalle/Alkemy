64ee4ddb5de4de7986795abb4ac29bf1d8869542
aleanvalle <alexv4lle@gmail.com>
2024-02-08 10:57:06 -0300

    agregados ejerciciosDeClase

---
+++ MinisterioDeTrabajo_Java_Bootcamp/EjerciciosEnClase/src/clase2/Ejercicio1.java
@@ -0,0 +1,13 @@
+package clase2;
+
+public class Ejercicio1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int identificador = 38995802;
+		boolean estudiante = true;
+		String apellido = "Vallejos";
+		System.out.println(identificador + ", " + estudiante + ", " + apellido + " Alexis");
+	}
+
+}
---
+++ MinisterioDeTrabajo_Java_Bootcamp/EjerciciosEnClase/src/clase2/Ejercicio2.java
@@ -0,0 +1,24 @@
+package clase2;
+
+public class Ejercicio2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		byte unByte = 56;
+		short unShort = 32767;
+		int unInt = 999999;
+		long unLong = 23056902456l;
+		float unFloat = 2.5f;
+		double unDouble = 22.56;
+		char unCaracter = '&';
+		System.out.println(
+				unByte + ", "
+				+ unShort + ", "
+				+ unInt + ", "
+				+ unLong + ", "
+				+ unFloat + ", "
+				+ unDouble + ", "
+				+ unCaracter);
+	}
+
+}
