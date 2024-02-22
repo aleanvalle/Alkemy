2ca349edf9c10b2c55e547e18fe30bf47584a12d
aleanvalle <alexv4lle@gmail.com>
2024-02-22 11:03:43 -0300

    agregado veintidos_febrero_2024

---
+++ MinisterioDeTrabajo_Java_Bootcamp/veintidos_febrero_2024/main.java
@@ -0,0 +1,53 @@
+package veintidos_febrero_2024;
+
+import java.util.Scanner;
+
+public class main {
+
+	public static void main(String[] args) {
+		System.out.println("*Sum with For*\n");
+		System.out.println("Result:" + sum_for() + "\n");
+		System.out.println("*Sum with while*\n");
+		System.out.println("Result:" + while_loop() + "\n");
+		System.out.println("*Sum with do-while*\n");
+		System.out.println("Result:" + do_while_loop());
+	}
+	
+	public static int sum_for() 
+	{
+		int sum = 0;
+		for(int i = 1; i < 11; i++) 
+		{
+			sum += i;
+			System.out.println("Sum: " + sum + " Counter: " + i);
+		}
+		return sum;
+	}
+
+	public static int while_loop() 
+	{
+		int counter = 1;
+		int sum = 0;
+		while(counter < 11) 
+		{
+			sum += counter;
+			System.out.println("Sum: " + sum + " Counter: " + counter);
+			counter++;
+		}
+		return sum;
+	}
+	
+	public static int do_while_loop() 
+	{
+		int counter = 1;
+		int sum = 0;
+		do 
+		{
+			sum += counter;
+			System.out.println("Sum: " + sum + " Counter: " + counter);
+			counter++;
+		}while(counter < 11);
+		return sum;
+	}
+	
+}
